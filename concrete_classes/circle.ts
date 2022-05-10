635160ea55639f2ebe52c92c0453e955501a1e6c
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2022-05-10 15:38:29 -0300

    It fixes the calculus of the area of the circle.

--- concrete_classes/circle.ts
+++ concrete_classes/circle.ts
@@ -2,6 +2,6 @@ import { Shape } from "../common_interfaces/shape";
 
 export class Circle implements Shape {
   calculateArea(value: number): number {
-    return +(Math.PI * value).toFixed(2);
+    return +(Math.PI * Math.pow(value, 2)).toFixed(2);
   }
 }
