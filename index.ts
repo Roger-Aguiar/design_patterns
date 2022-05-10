e08ceff24ea47a020075f28cb016061564eb4ee9
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2022-05-10 16:00:37 -0300

    It creates a folder called Factory in order to separate each design pattern.

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-import { ConcreteCircle } from "./concrete_creator_classes/concrete.circle";
-import { ConcreteSquare } from "./concrete_creator_classes/concrete.square";
-import { ShapeCreator } from "./creator/shape.creator";
+import { ConcreteCircle } from "./Factory/concrete_creator_classes/concrete.circle";
+import { ConcreteSquare } from "./Factory/concrete_creator_classes/concrete.square";
+import { ShapeCreator } from "./Factory/creator/shape.creator";
 
-function execute(creator: ShapeCreator) {
+function executeFactory(creator: ShapeCreator) {
     let area: number = creator.createShape(5);
     console.log(`Area: ${area.toString()}`);
 }
 
 console.log("It displays the area of a square with sides equal 5.");
-execute(new ConcreteSquare());
+executeFactory(new ConcreteSquare());
 
 console.log("\nIt displays the area of a circle with radius equal 5.");
-execute(new ConcreteCircle());
+executeFactory(new ConcreteCircle());
