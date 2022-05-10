5c06f64f72badbf4e520bea0f3f8e49b84b90aa6
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2022-05-10 15:29:21 -0300

    It updates the return to use a Math.pow function.

--- concrete_classes/square.ts
+++ concrete_classes/square.ts
@@ -2,6 +2,6 @@ import { Shape } from "../common_interfaces/shape";
 
 export class Square implements Shape {
   calculateArea(value: number): number {
-    return value * value;
+    return Math.pow(value, 2);
   }
 }
