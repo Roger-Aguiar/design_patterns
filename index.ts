9f7cec47d171ab79c0ef6ce712b073f09890c63f
Roger <roger.silvaaguiar.com.br>
2022-05-10 19:17:21 -0300

    It adds param to executeFactory function.

--- index.ts
+++ index.ts
@@ -2,13 +2,13 @@ import { ConcreteCircle } from "./Factory/concrete_creator_classes/concrete.circ
 import { ConcreteSquare } from "./Factory/concrete_creator_classes/concrete.square";
 import { ShapeCreator } from "./Factory/creator/shape.creator";
 
-function executeFactory(creator: ShapeCreator) {
-    let area: number = creator.createShape(5);
+function executeFactory(creator: ShapeCreator, value: number) {
+    let area: number = creator.createShape(value);
     console.log(`Area: ${area.toString()}`);
 }
 
-console.log("It displays the area of a square with sides equal 5.");
-executeFactory(new ConcreteSquare());
+console.log("It displays the area of a square with sides equal 9.");
+executeFactory(new ConcreteSquare(), 9);
 
-console.log("\nIt displays the area of a circle with radius equal 5.");
-executeFactory(new ConcreteCircle());
+console.log("\nIt displays the area of a circle with radius equal 9.");
+executeFactory(new ConcreteCircle(), 9);
